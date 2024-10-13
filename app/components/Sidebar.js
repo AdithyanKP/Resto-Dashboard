a68620a586a3d6ce5fe262d0e53c8305bc254f0d
AdithyanKP <adithyankp777@gmail.com>
2024-10-13 18:28:46 +0530

    fix: mobile resposive bug fix

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,42 +1,3 @@
-// "use client";
-// import Link from "next/link";
-// import { usePathname } from "next/navigation";
-
-// const Sidebar = () => {
-//   const pathName = usePathname();
-
-//   return (
-//     <div className="sidebar bg-gray-800 text-white h-screen w-64 flex flex-col">
-//       <div className="p-4 text-lg font-bold">My Restaurant</div>
-//       <ul className="flex-1 space-y-2 p-4">
-//         <li>
-//           <Link href="/">
-//             <span
-//               className={`block p-2 rounded transition ${
-//                 pathName === "/" ? "bg-gray-700" : "hover:bg-gray-700"
-//               }`}
-//             >
-//               Dashboard
-//             </span>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link href="/orders">
-//             <span className={`block p-2 rounded transition ${
-//                 pathName === "/orders" ? "bg-gray-700" : "hover:bg-gray-700"
-//               }`}>
-//               All Orders
-//             </span>
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
-
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -53,7 +14,6 @@ const Sidebar = () => {
 
   return (
     <div className="flex">
-      {/* Hamburger Menu for mobile screens */}
       <div className="sm:hidden p-4">
         <button
           onClick={toggleSidebar}
@@ -66,7 +26,7 @@ const Sidebar = () => {
       {/* Sidebar */}
       <div
         className={`bg-gray-800 text-white h-screen w-64 fixed sm:relative top-0 left-0 transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
+          isOpen ? "translate-x-0" : "-translate-x-full"
         } sm:translate-x-0 transition-transform duration-300 ease-in-out`}
       >
         <div className="p-4 text-lg font-bold">My Restaurant</div>
@@ -99,7 +59,7 @@ const Sidebar = () => {
       {/* Overlay to close the sidebar when open on mobile */}
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 sm:hidden"
+          className="inset-0 bg-black bg-opacity-50 sm:hidden"
           onClick={toggleSidebar}
         />
       )}
@@ -108,4 +68,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
